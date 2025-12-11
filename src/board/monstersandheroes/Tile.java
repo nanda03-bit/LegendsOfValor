25ec030e568cdb27515a9796130af440f8140fde
Nandana <nanda03@bu.edu>
2025-12-11 12:05:58 -0500

    Updated files

--- src/board/monstersandheroes/Tile.java
+++ src/board/monstersandheroes/Tile.java
@@ -1,12 +1,27 @@
+/**
+ * Filename: Tile.java
+ * Author: Nandana Shashi
+ * Date: 2025-Nov-09
+ * Description: Represents a single tile on the game board, storing its type
+ *              (COMMON, MARKET, or INACCESSIBLE) and providing access to tile information.
+ */
 package board.monstersandheroes;
 
 public class Tile {
     private TileType type;
 
+    /**
+     * Constructor for the Tile class.
+     * @param type The type of tile (COMMON, MARKET, or INACCESSIBLE).
+     */
     public Tile(TileType type) {
         this.type = type;
     }
 
+    /**
+     * Gets the type of this tile.
+     * @return The TileType of this tile.
+     */
     public TileType getType() {
         return type;
     }
--- src/board/monstersandheroes/TileType.java
+++ src/board/monstersandheroes/TileType.java
@@ -1,8 +1,27 @@
+/**
+ * Filename: TileType.java
+ * Author: Nandana Shashi
+ * Date: 2025-Nov-09
+ * Description: Enumeration representing the different types of tiles that can exist
+ *              on the game board. Used to distinguish between accessible areas,
+ *              markets, and blocked terrain.
+ */
 package board.monstersandheroes;
 
 public enum TileType {
+    /**
+     * Common space tile where battles can occur randomly.
+     */
     COMMON,
+    
+    /**
+     * Market tile where heroes can buy and sell items.
+     */
     MARKET,
+    
+    /**
+     * Inaccessible tile that cannot be entered by heroes.
+     */
     INACCESSIBLE
 }
 
